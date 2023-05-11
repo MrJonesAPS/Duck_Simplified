b4c11e509e1ee193f546526ebb2171b4b75aab48
Chris Jones <christopher.jones@apsva.us>
2023-05-11 14:05:07 -0400

    Removing a config file

--- https_admin/instance/config.py
+++
@@ -1,4 +0,0 @@
-import secrets
-SQLALCHEMY_DATABASE_URI = 'sqlite:////home/chris/Desktop/flask_app/http_student/instance/db.sqlite'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = secrets.token_hex(16)
